111966fd6393958a319ad53a34fe55218c317588
Abdul Raheem <raheemakhter787@gmail.com>
2026-06-23 03:27:31 +0500

    chore: remove large binaries and zip files from repo

--- web/electron/preload.cjs
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("streamVaultDesktop", {
-  openExternal: (url) => ipcRenderer.invoke("open-external-url", url),
-});
